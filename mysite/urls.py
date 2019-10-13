b8adbfa164793ab5d06e1ffdf27bc4ba00abcfb2
karlaG6 <karlaisabelgt@gmail.com>
2019-10-12 23:42:05 -0500

    ahora sí?

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.contrib.auth import views, logout
+from django.contrib.auth import views, logout, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
